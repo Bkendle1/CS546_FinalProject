72cb10e8c110133e257a85e6a30cd5e6d8ff2da5
Britt Li Kendle <90359619+Bkendle1@users.noreply.github.com>
2025-05-08 17:47:06 -0400

    Implemented addGachaCharacter to help seed database with gacha documents
    Added helper function that can add or subtract a user's ticket type by a given amount.

--- data/gacha-system.js
+++ data/gacha-system.js
@@ -1,8 +1,9 @@
 import * as helpers from "../helpers.js";
-import { users, collectionIndex } from "../config/mongoCollections.js";
+import { users, collectionIndex, gacha } from "../config/mongoCollections.js";
 import { ObjectId } from "mongodb";
 
-/*  Given the user's id, the size of the pull (should be either 1 or 5), and ticket type. Check if player has enough tickets to pull with. */
+/**  Given the user's id, the size of the pull (should be either 1 or 5), and ticket type. Check if player has enough tickets to pull with. Ticket type is case-insensitive.
+*/
 const canPull = async (userId, pullCount, ticketType) => {
     // check if userId is a valid string
     userId = helpers.validateString(userId, "User ID");
@@ -24,6 +25,7 @@ const canPull = async (userId, pullCount, ticketType) => {
 
     // verify that ticketType is a valid string
     ticketType = helpers.validateString(ticketType, "Ticket type");
+    ticketType = ticketType.toLowerCase();
 
     // check if user has enough normal tickets for pull
     const userCollection = await users();
@@ -49,15 +51,68 @@ const canPull = async (userId, pullCount, ticketType) => {
     }
 
 }
-try {
-    console.log(await canPull("67fbccf7bedaaf5edc00dae7", 50, "normal"));
-} catch (e) {
-    console.log(e);
+
+/** 
+ * Return the object whose id is the given characterId from the gacha collection. 
+ */
+const getGachaCharacterById = async (characterId) => {
+    // verify that characterId is a valid string
+    characterId = helpers.validateString(characterId, "Character IDd");
+    // verify that characterId is a valid ObjectID
+    if (!ObjectId.isValid(characterId)) {
+        throw "Invalid character ID.";
+    }
+    // get character from gacha collection
+    const gachaCollection = await gacha();
+    const gachaCharacter = await gachaCollection.findOne({ _id: ObjectId.createFromHexString(characterId) });
+
+    // if there's no character, throw
+    if (gachaCharacter === null) throw "No gacha character with that id.";
+    // return character object with its id as a string
+    gachaCharacter._id = gachaCharacter._id.toString();
+    return gachaCharacter;
 }
-// Normal Gacha pull function. Checks if the user can pull and if so, does a pull equal to the given pull count (should only either be 1 or 5) using the odds of a normal ticket. Update user's collection inventory to include new character assuming its not a duplicate.
+
+/**
+ * This function will check if the user can pull and if so, does a pull equal to the given pull count (should only either be 1 or 5) using the odds based on the pull type (normal or golden). It will also update the user's collection inventory to include the new character assuming its not a duplicate; otherwise, it will update the user’s currency amount to include the duplicate currency. Pull type is case-insensitive.
+ */
 export const gachaPull = async (userId, pullCount, pullType) => {
     // check if userId is a valid string
+    userId = helpers.validateString(userId, "User ID");
     // check if userId is a valid ObjectID
+    if (!ObjectId.isValid(userId)) {
+        throw "Invalid user ID.";
+    }
+    // check if pull count was provided
+    if (pullCount === undefined) {
+        throw "Pull count is missing.";
+    }
+    // check that pullCount is a whole number of at least 1
+    if (typeof (pullCount) !== 'number'
+        || pullCount < 1
+        || Number.isNaN(pullCount)
+        || !Number.isInteger(pullCount)) {
+        throw "Pull count has to be a positive whole number that's at least 1."
+    }
+    // check that pullType is valid string 
+    pullType = helpers.validateString(pullType, "Pull type");
+    // check if the pull type is a valid option
+    pullType = pullType.toLowerCase();
+    if (pullType !== "normal" || pullType !== "golden") {
+        throw "Pull type can only be either 'normal' or 'golden'.";
+    }
+
+    // check if user has enough tickets for this pull type (either golden or normal)
+    if (canPull(userId, pullCount, pullType)) {
+        // decrement user's corresponding ticket count 
+        helpers.updateTicketCount(userId, pullType, -pullCount);
+        // store pulled characters in an array
+        let pulledCharacters = [];
+        // do the given amount of pulls
+        for (let i = 0; i < pullCount; i++) {
+
+        }
+    }
 
     // update 'collected' field in collection index to true 
 }
@@ -66,16 +121,18 @@ export const gachaPull = async (userId, pullCount, pullType) => {
 
 // Check if pull is a duplicate. If so, give the user currency in exchange which varies depending on the rarity of the pulled character
 
-// Given a name, duplicate currency, and pull rate, add a character to the gacha system.
-
-
 // Update pull history with pulled character assuming its not a duplicate. Include the character's name, rarity, timestamp of pull, and image
 
 
-// Given a name, Add gacha character
-export const addCharacterToGacha = (name, pull_rate, duplicate_currency) => {
+/**
+ * Given a name, pull_rate, and duplicate currency, add a new character to the gacha system.
+ * IMPORTANT: Since characters will share the same _id across different collections, the new character must already exist in the collection index collection as that's where we copy the _id field from.
+ * Name is also case-insensitive
+*/
+export const addCharacterToGacha = async (name, pull_rate, duplicate_currency) => {
     // verify that name is a valid string
-    name = helpers.addCharacterToGacha(name, "Character name");
+    name = helpers.validateString(name, "Character name");
+    name = name.toLowerCase(); // name is case-insensitive
     // verify that pull rate is between 0 and 1
     if (typeof (pull_rate) !== 'number'
         || pull_rate < 0
@@ -85,13 +142,49 @@ export const addCharacterToGacha = (name, pull_rate, duplicate_currency) => {
     }
 
     // verify that duplicate currency is at least 1
+    if (typeof (duplicate_currency) !== 'number'
+        || duplicate_currency < 1
+        || Number.isNaN(duplicate_currency)
+        || !Number.isInteger(duplicate_currency)) {
+        throw "Duplicate currency has to be a positive whole number that's at least 1."
+    }
 
-}
-try {
+    // create new character entry for the gacha collection 
+    // NOTE: the new gacha character must already be in the collection index so we can copy over the ObjectId
+    const character_id = await helpers.getCharacterIdByName(name);
+    const newGachaCharacter = {
+        _id: ObjectId.createFromHexString(character_id),
+        name: name,
+        pull_rate: pull_rate,
+        duplicate_currency: duplicate_currency
+    }
 
-} catch (e) {
-    console.log(e);
+    const gachaCollection = await gacha(); // get reference to gacha collection
+    const insertInfo = await gachaCollection.insertOne(newGachaCharacter); // insert new character into gacha system
+    // if character wasn't created, throw
+    if (!insertInfo.acknowledged || !insertInfo.insertedId)
+        throw `Could not add ${name} to the gacha system.`;
+
+    const newId = insertInfo.insertedId.toString(); // get new character's id 
+    const gachaCharacter = await getGachaCharacterById(newId);     // get character object from gacha
+
+    gachaCharacter._id = gachaCharacter._id.toString();     // convert new character id to string before returning object
+
+    return gachaCharacter; // return object of new gacha character
 }
+
+// try {
+//     console.log(await addCharacterToGacha("Leomon", 0.5, 400));
+// } catch (e) {
+//     console.log(e);
+// }
+
+
+// try {
+//     console.log(await addCharacterToGacha("deeznuts", 0.5, 1.));
+// } catch (e) {
+//     console.log(e);
+// }
 
 
 // try {
@@ -100,3 +193,8 @@ try {
 //     console.log(e);
 // }
 
+// try {
+//     console.log(await canPull("67fbccf7bedaaf5edc00dae7", 1, "normal"));
+// } catch (e) {
+//     console.log(e);
+// }
--- helpers.js
+++ helpers.js
@@ -1,5 +1,5 @@
 import { users, collectionIndex, gacha } from "./config/mongoCollections.js";
-
+import { ObjectId } from "mongodb";
 /**
  * Verifies that the given string is not undefined, empty, not of type string, nor just whitespace, and also returns the string trimmed with trim().
  */
@@ -15,9 +15,9 @@ export const validateString = (str, varName) => {
 }
 
 /**
- * Given a character's name, retrieve their character_id from the collection index collection (i.e. the collection for our character index) basedon that name.
+ * Given a character's name, returns their character_id as a string from the collection index collection (i.e. the collection for our character index) based on that name.
  */
-export const getCharacterId = async (name) => {
+export const getCharacterIdByName = async (name) => {
     // verify that name is valid string
     name = validateString(name, "Character name");
 
@@ -28,4 +28,50 @@ export const getCharacterId = async (name) => {
         throw "No character with that name.";
     }
     return character._id.toString(); // return string of character id
+}
+
+/**
+ * Given a user's id, increment or decrement their current count of the given ticketType by a given amount. Decrements should be negative. Ticket type is case insensitive. If the decremented value would bring the ticket count to be smaller than 0, the count stays 0.
+ */
+export const updateTicketCount = async (userId, ticketType, amount) => {
+    // verify that userId is a valid string
+    userId = validateString(userId, "User ID");
+    // verify that userId is a valid ObjectId
+    if (!ObjectId.isValid(userId)) {
+        throw "Invalid user ID.";
+    }
+    // verify that ticketType is a valid string
+    ticketType = validateString(ticketType, "Ticket type");
+    ticketType = ticketType.toLowerCase(); // ticket type should be case-insensitive
+    // verify that amount was provided
+    if (amount === undefined) {
+        throw "Missing ticket amount.";
+    }
+    // verify that amount is a valid whole number 
+    if (typeof (amount) !== 'number'
+        || !Number.isInteger(amount)
+        || Number.isNaN(amount)) {
+        throw "Ticket amount must be a valid whole number."
+    }
+
+    // get reference to user
+    const userCollection = await users();
+    const user = await userCollection.findOne({ _id: ObjectId.createFromHexString(userId) });
+    // if user doesn't exist, throw
+    if (!user) {
+        throw "User does not exist."
+    }
+
+    // otherwise, update the ticketType count by the given amount
+    if (ticketType === 'normal') {
+        const newCount = Math.max(0, user.metadata.ticket_count.normal += amount); // compute new ticket count 
+        await userCollection.updateOne({ _id: ObjectId.createFromHexString(userId) }, { $set: { "metadata.ticket_count.normal": newCount } });
+        return `${user.username} now has ${newCount} normal tickets.`
+    } else if (ticketType === 'golden') {
+        const newCount = Math.max(0, user.metadata.ticket_count.golden += amount); // compute new ticket count
+        await userCollection.updateOne({ _id: ObjectId.createFromHexString(userId) }, { $set: { "metadata.ticket_count.golden": newCount } });
+        return `${user.username} now has ${newCount} golden tickets.`
+    } else {
+        throw "Ticket type must be 'normal' or 'golden'.";
+    }
 }
