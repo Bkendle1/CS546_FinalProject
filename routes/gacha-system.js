0c03030f14e0179bbd99eb0ab3efe4bf7936b4db
Britt Li Kendle <90359619+Bkendle1@users.noreply.github.com>
2025-05-09 11:56:08 -0400

    Started on routes for gacha.

---
+++ routes/gacha-system.js
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+const router = Router();
+import { gachaData } from '../data/index.js';
+import * as helpers from '../helpers.js';
+import { ObjectId } from 'mongodb';
+
+router
+    .route('/')
+    .get(async (req, res) => {
+        // render a partial for a script that runs the phaser scene for the gacha compnent
+    });
+
+router
+    .route('/normal')
+    .get(async (req, res) => {
+        // a single, normal pull
+    });
+router
+    .route('/normal/bulk')
+    .get(async (req, res) => {
+        // a bulk, normal pull
+    });
+
+
+router
+    .route('/golden')
+    .get(async (req, res) => {
+        // a single, golden pull
+    });
+
+router
+    .route('/golden/bulk')
+    .get(async (req, res) => {
+        // a bulk, golden pull
+    });
+export default router;
