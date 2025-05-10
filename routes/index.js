2ecb0f51933474335ca153398884b914e031980d
NickkWilsonn <nicholaswilson913@gmail.com>
2025-05-10 16:32:54 -0400

    imported collection index routes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 import userRoutes from "./user.js";
 import shopRoutes from "./shop.js";
+import indexRoutes from "./collectionIndex.js"
 // import all the other routes ...
 
 const configRouteFunction = (app) => {
-  app.use("/", userRoutes);
-  app.use("/shop", shopRoutes);
+    app.use("/", userRoutes);
+    app.use("/shop", shopRoutes);
+    app.use("/collectionIndex", indexRoutes);
 
-  app.use(/(.*)/, (req, res) => {
-    res.status(404).json({ error: "Route Not found" });
-  });
+    app.use(/(.*)/, (req, res) => {
+        res.status(404).json({ error: "Route Not found" });
+    });
 };
 
 export default configRouteFunction;
