2b83cb44d1fa7b0c85ed1b7474c1f96b5d3ffc37
NickkWilsonn <nicholaswilson913@gmail.com>
2025-05-10 16:47:59 -0400

    implemented client-side collectionIndex using kaplay

---
+++ public/js/collectionIndex.js
@@ -0,0 +1,93 @@
+import kaplay from "https://unpkg.com/kaplay@3001/dist/kaplay.mjs";
+
+async function fetchEntries() {
+    const res = await fetch("/collectionIndex/entries");
+    if (!res.ok) {
+        throw "Error: Fetch failed- " + res.status;
+    }
+    return res.json();
+}
+
+function setupIndex(entries, canvas) {
+    const cols = Math.ceil(Math.sqrt(entries.length));
+    const spacingX = canvas.width  / cols;
+    const spacingY = canvas.height / cols;
+
+    entries.forEach((e, idx) => {
+        const row = Math.floor(idx / cols);
+        const col = idx % cols;
+        const x = spacingX * col + spacingX/2;
+        const y = spacingY * row + spacingY/2;
+
+        // character sprite
+        add([
+            sprite(e._id),
+            pos(x, y),
+            area(),
+            layer("sprite")
+        ]);
+
+        // name
+        add([
+            drawText(e.name),
+            pos(x - spacingX/4, y + spacingY/4),
+            layer("ui")
+        ]);
+
+        // rarity
+        add([
+            drawText(e.rarity),
+            pos(x - spacingX/4, y + spacingY/4 + 20),
+            layer("ui")
+        ]);
+
+        /* // image
+        add([
+            drawText(e.image),
+            pos(x - spacingX/4, y + spacingY/4),
+            layer("ui")
+        ]); */
+
+        // description
+        add([
+            drawText(e.description),
+            pos(x - spacingX/4, y + spacingY/4),
+            layer("ui")
+        ]);
+
+        // collected flag
+        if (e.collected) {
+            add([
+                drawText("✔️"),
+                pos(x + spacingX/4, y - spacingY/4),
+                layer("ui")
+            ]);
+        }
+    });
+}
+
+window.addEventListener("DOMContentLoaded", main);
+async function main() {
+    let entries;
+    try {
+        entries = await fetchEntries();
+    } catch (e) {
+        console.error(e);
+        return;
+    }
+
+    // Initialize Kaplay
+    const canvas = document.getElementById("index-canvas");
+    kaplay({ 
+        canvas, 
+        width: canvas.width, 
+        height: canvas.height 
+    });
+
+    // preload each image under a key of its _id
+    for (const e of entries) {
+        loadSprite(e._id, e.image);
+    }
+
+    setupIndex(entries, canvas);
+};
