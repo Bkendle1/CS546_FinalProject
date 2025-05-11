e6f2ac4d7714f3b4e82f43cb92aa86d8cf8dac68
NickkWilsonn <nicholaswilson913@gmail.com>
2025-05-11 16:06:06 -0400

    worked on seeding gacha, collectionIndex, and shop collections

--- tasks/seed.js
+++ tasks/seed.js
@@ -4,76 +4,213 @@ import {
     shopData, 
     indexData
 } from "../data/index.js";
+import {
+    rarityToPullRate,
+    rarityToDupCurrency
+} from "../helpers.js";
 
-async function main() {
+const main = async () => {
     const db = await dbConnection();
+    console.log("Connected to Gacha_Game db:", db.databaseName);
+
     // await db.dropDatabase();
     // console.log("Dropped existing database.");
 
-    // 1) seed collection index
+    // 1) create characters
     const characters = [
+        // character 1
         {
-            name: "Leomon",
-            rarity: "Legendary",
-            image: "image.jpg",
-            description: "Leomon description",
-            pull_rate: 0.2387,
-            duplicate_currency: 745
+            // id: 
+            name: "Koromon",
+            rarity: "Rare",
+            // image: "",
+            description: "Koromon has shed its fur and grown one size bigger. It can move a bit faster, but fighting is still too much for it. It threatens enemies with bubbles from its mouth.",
         },
+        // character 2
         {
-            name: "Witchmon",
+            // id 
+            name: "Tsunomon",
+            rarity: "Rare",
+            // image: "",
+            description: "A small Digimon, but with one of the tentacles on its head hardened. It is covered in tufts of bushy hair. Its playful and mischievous personality remains, but it has yet to awaken to its combat ability.",
+        },
+        // character 3
+        {
+            // id 
+            name: "Tokomon",
+            rarity: "Uncommon",
+            // image: "",
+            description: "A small Digimon with strange appendages growing under its body. In-training Digimon with legs are quite rare, and this one's cute as a button to boot. Don't let its looks fool you, though; carelessly reach out to it and expect a big set of chompers to clamp down on your hand. That being said, it doesn't have an ounce of ill will in its body.", 
+        },
+        // character 4
+        {
+            // id 
+            name: "Tanemon",
+            rarity: "Common",
+            // image: "",
+            description: "A bulb Digimon with sprout-like objects budding from its head. When an enemy draws close, this big old coward digs a hole with its four legs and buries its body in the ground. When it moves underground, the objects on its head mimic vegetation, protecting it from opponents.", 
+        },
+        // character 5
+        {
+            // id 
+            name: "Pagumon",
+            rarity: "Uncommon",
+            // image: "",
+            description: "This small Digimon can fly at low altitudes with the ear-like protrusions growing out of its head. It can move these ears like a second pair of hands, using them to mock the enemy with all kinds of gestures. It has a malicious streak, often chasing after Digimon like Koromon or Tsunomon and teasing them. It also utilizes sprays of poison bubbles towards this end.",
+        },
+        // character 6
+        {
+            // id 
+            name: "Motimon",
+            rarity: "Common",
+            // image: "",
+            description: "A soft-bodied Digimon possessing an elastic outer skin, toddling along on the bumps under its torso. When it gets worked up, its body swells up like a mochi rice cake, earning it the name Motimon. Its appearance belies an above-average intelligence, which leads to the assumption that it was born from a computer's dictionary function. It understands human speech, and can sometimes be seen transforming its body to communicate.", 
+        },
+        // character 7
+        {
+            // id 
+            name: "Bukamon",
+            rarity: "Common",
+            // image: "",
+            description: "This zany Digimon looks like a baby aquatic dinosaur, and moves like a sprightly seahorse. It tends to run away when others get close. Its exodermis can't withstand deep-sea water pressure or low temperatures, so it can't dive in the ocean depths for long.", 
+        },
+        // character 8
+        {
+            // id 
+            name: "Peti Meramon",
+            rarity: "Uncommon",
+            // image: "",
+            description: "Its entire body is ablaze due to how its Digicore burns fiercely in the heart of its small body. It has a fierce, aggressive personality atypical of small Digimon, and will spit out small bullets of fire from its mouth to threaten any enemy it encounters.",
+        },
+        // character 9
+        {
+            // id 
+            name: "Pyocomon",
+            rarity: "Common",
+            // image: "",
+            description: "A Bulb-type Lesser Digimon with a large flower blooming from its head. It is able to move by skillfully operating its root-like tentacles, and with its lightness, it can rise into the air, but only to a small height. As it is brimming with curiosity, it is restlessly stirring and its appearance seems very cute. It has a habit of living together in flocks, and it is said that due to grouping, the flocks will grow from a few to a few hundred.",
+        },
+        // character 10
+        {
+            // id 
+            name: "Kapurimon",
+            rarity: "Common",
+            // image: "",
+            description: "It is a tiny Digimon wearing a metallic helmet. Within the two horns attached to its helmet, it has antennae that are able to receive any radio wave or sound. Kapurimon is in possession of bat-like traits, as it has weak eyesight, and produces ultrasonic waves from its mouth, recognizing the objects ahead of it from the rebounding sound wave.",
+            // pull_rate: ,
+            // duplicate_currency: 
+        },
+        // character 11
+        {
+            // id 
+            name: "Nyaromon",
+            rarity: "Common",
+            // image: "",
+            description: "A small Digimon with feline characteristics. Its capricious, cat-like behavior earned it the name Nyaromon. While both curious and fickle, it has a lonely side to it as well.",
+        },
+        // character 12
+        {
+            // id 
+            name: "DemiVeemon",
             rarity: "Legendary",
-            image: "image.jpg",
-            description: "Leomon description",
-            pull_rate: 0.2387,
-            duplicate_currency: 745
+            // image: "",
+            description: "Unusually for an In-Training Digimon, it possesses a body, hands, and feet, and so is able to grip objects with both of its small hands, and move while hopping about with both feet. As it is very voracious, it especially likes sweet foods. Also, as it is very fond of sleeping, if you take your eyes off of it, it will instantly fall asleep.",
+        },
+        // character 13
+        {
+            // id 
+            name: "Poromon",
+            rarity: "Common",
+            // image: "",
+            description: "A Small Bird Digimon that can fly at low altitudes. It has small feathers growing closely packed on its body, and unusually for a Baby Digimon, it has wings. Its most charming feature is the feather ornament attached to its head, so it maintains it intently. It has a fundamentally tidy personality, but because it cannot turn its neck(?) even if it wants to groom itself, it relies on its comrades.",
+        },
+        // character 14
+        {
+            // id 
+            name: "Upamon",
+            rarity: "Common",
+            // image: "",
+            description: "Upamon is an Amphibian Digimon. It has ear-like branchia growing from both sides of its head. Although it is able to live whether on land or underwater, it appears that it is rather suited for life on land. Because of its carefree personality, it is easily and frequently bullied by other Digimon. However, because of its personality, there are occasions when it mistakes it as being played with, rather than it being bullied.",
+        },
+        // character 15
+        {
+            // id 
+            name: "Minomon",
+            rarity: "Common",
+            // image: "",
+            description: "On days with good weather, or when it's in a good mood, it sticks its face out of its shell and vegetates. It hangs onto high places with the ivy (hair?) growing from its head, and moves by hanging onto large Digimon, but if it's over a short distance, it is also able to move by floating through the air. Due to its vegetative personality, it has a fault of not easily showing its emotions on its face. Also, as solitude is its 'specialty', it has the inner fortitude to not be discouraged even if it gets worried and anxious. Its Special Move is throwing a hard, pine cone-shaped substance.",
+        },
+        // character 16
+        {
+            // id 
+            name: "Gummymon",
+            rarity: "Uncommon",
+            // image: "",
+            description: "It has an exceedingly energetic personality, both cheerful and lively. Although it is growing, it's squishy because its body tissue is as unstable as usual but its horn portion is hardened, and it intimidates the opponent with headbutts.",
         },
     ]
-    await gachaData.addCharacterToGacha("Elecmon", 0.6721, 212);
-    await gachaData.addCharacterToGacha("Witchmon", 0.0953, 891);
-    await gachaData.addCharacterToGacha("Igamon", 0.8412, 673);
-    await gachaData.addCharacterToGacha("Troopmon", 0.4198, 304);
-    await gachaData.addCharacterToGacha("Pteranomon", 0.3875, 158);
-    await gachaData.addCharacterToGacha("Sagittarimon", 0.7124, 996);
-    await gachaData.addCharacterToGacha("Chibickmon", 0.0037, 587);
-    await gachaData.addCharacterToGacha("Hanumon", 0.5609, 423);
-    await gachaData.addCharacterToGacha("Shoutmon", 0.9981, 339);
 
-    console.log("Done seeding gacha collection.");
-
-    // shop items 
-    await shopData.addItemToShop(
-        "normal ticket",
-        10,
-        "A basic ticket good for one Gacha pull.",
-        // normal_ticket image link
-    );
-    await shopData.addItemToShop(
-        "golden ticket",
-        100,
-        "A golden ticket with higher odds for rare characters.",
-        // golden_ticket image link
-    );
-    await shopData.addItemToShop(
-        "food",
-        50,
-        "Food to feed your characters and boost their experience.",
-        // food image link
-    );
-
-    console.log("Done seeding shop collection.");
-
-    // collection index
-    /* const indexIds = {};
-    for (const c of chars) {
-        const id = await indexData.addIndexEntry (
+    // 2) seed collection index
+    console.log("Starting to seed collectionIndex...");
+    for (const c of characters) {
+        await indexData.addIndexEntry (
             c.name,
             c.rarity,
             c.image,
             c.description
         );
-        indexIds[c.name] = id;
-    } */
+    }
+    console.log("Done seeding collectionIndex!");
 
+    // 3) seed gacha
+    console.log("Starting to seed gacha collection...");
+    for (const c of characters) {
+        const pullRate = rarityToPullRate(c.rarity);
+        const dupCur   = rarityToDupCurrency(c.rarity);
+        await gachaData.addCharacterToGacha(
+            c.name,
+            pullRate,
+            dupCur
+        );
+    }
+    console.log("Done seeding gacha!");
+
+    // 4) create shop items
+    const shopItems = [
+        {
+            name: "normal ticket",
+            cost: 10,
+            description: "A basic ticket good for one Gacha pull.",
+            // image: ""
+        },
+        {
+            name: "golden ticket",
+            cost: 100,
+            description: "A golden ticket with higher odds for rare characters.",
+            // image: ""
+        },
+        {
+            name: "food",
+            cost: 50,
+            description: "Food to feed your characters and boost their experience.",
+            // image: ""
+        },
+    ]
+
+    // 5) seed shop
+    console.log("Starting to seed shop collection...");
+    for (const item of shopItems) {
+        await shopData.addItemToShop(
+            item.name,
+            item.cost,
+            item.description,
+            item.image
+        );
+    }
+    console.log("Done seeding shop!");
+
+    console.log("Done seeding all collections!!");
     await closeConnection();
 }
+
+main();
